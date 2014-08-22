081ac1280cda4b3e7f291d4900ab69df03798f39
Jan Schulte <jan@janschulte.com>
2014-08-22 18:08:19 +0200

    Added node parsing & fixed test cases. Parser now working for nodes, ways & relations

--- parser/src/test/scala/org/geow/osm/parser/OsmObjectParserTest.scala
+++ parser/src/test/scala/org/geow/osm/parser/OsmObjectParserTest.scala
@@ -17,19 +17,47 @@ import org.scalacheck.Arbitrary.arbitrary
 import scala.io.Source
 import org.geow.osm.parser.OsmObjectParser
 import org.geow.osm.parser.OsmObjectParser._
-import org.geow.model.OsmWay
-import org.geow.model.OsmProperties
-import org.geow.model.OsmTag
-import org.geow.model.OsmMember
-import org.geow.model.OsmTypeWay
-import org.geow.model.OsmRoleOuter
-import org.geow.model.OsmRelation
+import org.geow.model._
+import org.geow.model.geometry.OsmPoint
 
 @RunWith(classOf[JUnitRunner])
 class OsmObjectParserTest extends Specification with ScalaCheck {
 
-  val relationXML =
+  val xml =
     <osm version="0.6" generator="CGImap 0.3.3 (4443 thorn-02.openstreetmap.org)" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
+      <node id="2465725143" visible="true" version="2" changeset="21736329" timestamp="2014-04-16T19:23:01Z" user="black_bike" uid="18130" lat="51.2378913" lon="6.7797524">
+        <tag k="addr:city" v="Düsseldorf"/>
+        <tag k="addr:country" v="DE"/>
+        <tag k="addr:housenumber" v="53"/>
+        <tag k="addr:postcode" v="40477"/>
+        <tag k="addr:street" v="Nordstraße"/>
+        <tag k="amenity" v="restaurant"/>
+        <tag k="cuisine" v="regional"/>
+        <tag k="name" v="Himmel und Ähd"/>
+        <tag k="phone" v="+49 211 4981361"/>
+        <tag k="website" v="http://www.himmel-aehd.de"/>
+      </node>
+      <way id="143653722" visible="true" version="5" changeset="19982704" timestamp="2014-01-14T00:57:49Z" user="teufli" uid="247886">
+        <nd ref="203790573"/>
+        <nd ref="717638289"/>
+        <nd ref="73664701"/>
+        <nd ref="827539061"/>
+        <nd ref="717638282"/>
+        <nd ref="827538924"/>
+        <nd ref="73664703"/>
+        <nd ref="717638284"/>
+        <nd ref="717638278"/>
+        <tag k="highway" v="trunk"/>
+        <tag k="lanes" v="2"/>
+        <tag k="layer" v="-1"/>
+        <tag k="lit" v="yes"/>
+        <tag k="lit_by_gaslight" v="no"/>
+        <tag k="maxspeed" v="80"/>
+        <tag k="maxspeed:conditional" v="60 @ (22:00-06:00)"/>
+        <tag k="motorroad" v="yes"/>
+        <tag k="name" v="Rheinalleetunnel"/>
+        <tag k="oneway" v="yes"/>
+      </way>
       <relation id="91062" visible="true" version="11" changeset="18781052" timestamp="2013-11-08T12:20:08Z" user="Gehrke" uid="14002">
         <member type="way" ref="245181859" role="outer"/>
         <member type="way" ref="245181864" role="outer"/>
@@ -47,6 +75,26 @@ class OsmObjectParserTest extends Specification with ScalaCheck {
         <tag k="wikipedia" v="de:Golzheim_(Düsseldorf)"/>
       </relation>
     </osm>
+
+  val nodePoint = OsmPoint(6.7797524, 51.2378913)
+
+  val nodeProps = OsmProperties(2465725143L, "black_bike", 18130L,
+    convertXmlDateToLong("2014-04-16T19:23:01Z"), true, 2,
+    21736329)
+
+  val nodeTags = List(
+    OsmTag("addr:city", "Düsseldorf"),
+    OsmTag("addr:country", "DE"),
+    OsmTag("addr:housenumber", "53"),
+    OsmTag("addr:postcode", "40477"),
+    OsmTag("addr:street", "Nordstraße"),
+    OsmTag("amenity", "restaurant"),
+    OsmTag("cuisine", "regional"),
+    OsmTag("name", "Himmel und Ähd"),
+    OsmTag("phone", "+49 211 4981361"),
+    OsmTag("website", "http://www.himmel-aehd.de"))
+    
+    val node = OsmNode(nodeProps, nodeTags, nodePoint)
 
   val relationMembers = List(
     OsmMember(OsmTypeWay, 245181859L, OsmRoleOuter),
@@ -71,31 +119,6 @@ class OsmObjectParserTest extends Specification with ScalaCheck {
 
   val relation = OsmRelation(relationProps, relationTags, relationMembers)
 
-  val wayXML =
-    <osm version="0.6" generator="CGImap 0.3.3 (5353 thorn-02.openstreetmap.org)" copyright="OpenStreetMap and contributors" attribution="http://www.openstreetmap.org/copyright" license="http://opendatacommons.org/licenses/odbl/1-0/">
-      <way id="143653722" visible="true" version="5" changeset="19982704" timestamp="2014-01-14T00:57:49Z" user="teufli" uid="247886">
-        <nd ref="203790573"/>
-        <nd ref="717638289"/>
-        <nd ref="73664701"/>
-        <nd ref="827539061"/>
-        <nd ref="717638282"/>
-        <nd ref="827538924"/>
-        <nd ref="73664703"/>
-        <nd ref="717638284"/>
-        <nd ref="717638278"/>
-        <tag k="highway" v="trunk"/>
-        <tag k="lanes" v="2"/>
-        <tag k="layer" v="-1"/>
-        <tag k="lit" v="yes"/>
-        <tag k="lit_by_gaslight" v="no"/>
-        <tag k="maxspeed" v="80"/>
-        <tag k="maxspeed:conditional" v="60 @ (22:00-06:00)"/>
-        <tag k="motorroad" v="yes"/>
-        <tag k="name" v="Rheinalleetunnel"/>
-        <tag k="oneway" v="yes"/>
-      </way>
-    </osm>
-
   val wayProps = OsmProperties(143653722L, "teufli", 247886L, convertXmlDateToLong("2014-01-14T00:57:49Z"), true, 5,
     19982704)
   val wayNds = List(203790573L, 717638289L, 73664701L, 827539061L, 717638282L, 827538924L, 73664703L, 717638284L, 717638278L)
@@ -114,24 +137,18 @@ class OsmObjectParserTest extends Specification with ScalaCheck {
 
   val way = OsmWay(wayProps, wayTags, wayNds)
 
-  val relationSource = Source.fromString(relationXML.toString)
-
-  val waySource = Source.fromString(wayXML.toString)
+  val parser = new OsmObjectParser(Source.fromString(xml.toString))
 
   "The OsmObjectParser" should {
 
-    "parse an OsmRelation" ! check {
-      val parser = new OsmObjectParser(relationSource)
+    "parse an Osm xml" ! check {
+      parser.hasNext must be_==(true)
+      parser.next must beSome(node)
+      parser.hasNext must be_==(true)
+      parser.next must beSome(way)
       parser.hasNext must be_==(true)
       parser.next must beSome(relation)
     }
-    "parse an OsmWay" ! check {
-      val parser = new OsmObjectParser(waySource)
-      parser.hasNext must be_==(true)
-      parser.next must beSome(way)
-
-    }
-
   }
 
 }
